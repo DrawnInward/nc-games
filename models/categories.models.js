4d335ba6a77361d864b93365445f9f928d136d2f
DrawnInward <jonathan_bean@hotmail.co.uk>
2023-05-09 14:14:33 +0100

    completed api/catergories

---
+++ models/categories.models.js
@@ -0,0 +1,16 @@
+const db = require("../db/connection");
+
+exports.selectCategories = () => {
+  const query = `SELECT * FROM categories`;
+  return db
+    .query(query)
+    .then((result) => {
+      return result.rows;
+    })
+    .catch((err) => {
+      return Promise.reject({
+        status: 500,
+        msg: "failed to select catergories",
+      });
+    });
+};
