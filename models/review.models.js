a5f663cec40957c9bed35a7774f53f882497f4d1
DrawnInward <jonathan_bean@hotmail.co.uk>
2023-05-12 12:23:37 +0100

    completed ticket 12

--- models/review.models.js
+++ models/review.models.js
@@ -1,14 +1,17 @@
 const db = require("../db/connection");
+const { checkFieldExists } = require("../db/seeds/utils");
 
 exports.selectReview = (id) => {
   const reviewQuery = `
-    SELECT * FROM reviews
-    WHERE review_id = $1`;
+  SELECT reviews.owner, reviews.title, reviews.review_id, reviews.category, reviews.review_img_url, reviews.created_at, reviews.votes, reviews.designer, review_body, COUNT(comments.comment_id) comment_count
+  FROM reviews
+  LEFT JOIN comments ON comments.review_id = reviews.review_id
+  WHERE reviews.review_id = $1
+  GROUP BY reviews.owner, reviews.title, reviews.review_id, reviews.category, reviews.review_img_url, reviews.created_at, reviews.votes, reviews.designer, review_body;`;
 
-  return db.query(reviewQuery, [id]).then((result) => {
-    if (result.rows.length === 0) {
-      return Promise.reject({ status: 404, msg: "review not found" });
-    }
-    return result.rows[0];
+  return checkFieldExists("reviews", "review_id", id).then(() => {
+    return db.query(reviewQuery, [id]).then((result) => {
+      return result.rows[0];
+    });
   });
 };
