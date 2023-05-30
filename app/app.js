2e37aca42aada4623bbbef042d2f1dc212fcd828
DrawnInward <jonathan_bean@hotmail.co.uk>
2023-05-30 09:43:10 +0100

    allow cross origin resource sharing

--- app/app.js
+++ app/app.js
@@ -5,9 +5,11 @@ const {
   catchAllError,
 } = require("./errorHandling");
 const apiRouter = require("./routers/api.route");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/api", apiRouter);
 
